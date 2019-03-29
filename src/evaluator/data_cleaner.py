30dea6531bf2b1ed6c066ef7c27ffc556d543c7d
Niklas <niklas.nevalainen@gmail.com>
2019-03-29 14:19:33 +0200

    Create function for cleaning fetched pages

---
+++ src/evaluator/data_cleaner.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import json
+
+def clean_file(src, dest):
+
+    with open(src, 'r') as rfp:
+        json_obj = json.load(rfp)
+    
+    wikitext = json_obj['parse']['wikitext']['*']
+    
+    with open(dest, 'w') as wfp:
+        wfp.write(wikitext)
+
+def create_cleaned_files(src_dir, dest_dir):
+
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    for filename in os.listdir(src_dir):
+
+        fname, ext = os.path.splitext(filename)
+
+        src = os.path.join(src_dir, filename)
+        dest = os.path.join(dest_dir, fname + '.txt')
+        clean_file(src, dest)
+    
+def main():
+
+    src_dir = sys.argv[1]
+    dest_dir = sys.argv[2]
+
+    create_cleaned_files(src_dir, dest_dir)
+
+main()
